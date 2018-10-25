96f2e40269f349402d8c6820a235729ce7374f24
Ash <ygshah@asu.edu>
2018-10-24 21:54:01 -0700

    Update mespp.py

--- mespp.py
+++ mespp.py
@@ -1,27 +1,89 @@
-''' Sequential Allocation MESPP algorithm
-
-Input: Multi-agent efficient search problem
- % V C N' is the set of nodes visited by the searchers
- V= [] initially
- for all searchers k do
- % Ak C N' is a feasible path for searcher k
- % Finding the arg max solves the ESPP for searcher k
- Ak = arg max over Ak the value of F (V U Ak)
- V = V U Ak
- end for
- Return Ak for all searchers k '''
+import math
 import numpy as np
-import random as rd
-capture_state = False
-V = []   # This is the set of nodes visited by the searchers
-N = [[0.5, 0.5, 0.0], [0.3,0.3,0.3], [0.0, 0.5, 0.5]]
-N_mat = np.matrix(N)
-print (N_mat)
-def __init__ (self, searcher):
-     self.searcher = searcher
-def captured (searcher):
-    for k in range(0,len(searcher)):
-        Ak = []
-        if j not in Ak:
-            Ak.append(j)
-            if Ak
+S = [2,0]
+
+T = [0,2]
+
+h = [[2,1,0],[3,20,1],[4,20,1]]
+#h_matrix = np.matrix(h)
+
+
+
+X =2
+Y =2
+neighbors = lambda x, y : [(x2, y2) for x2 in range(x-1, x+2)
+                                   for y2 in range(y-1, y+2)
+                                   if (-1 < x <= X and
+                                       -1 < y <= Y and
+                                       (x != x2 or y != y2) and
+                                       (0 <= x2 <= X) and
+                                       (0 <= y2 <= Y))]
+def manhattan_distance (start,neigh):
+    distance = abs(start[0]-neigh[0])+ abs(start[1]-neigh[1])
+
+    return distance
+def choose_neighbor(start):
+    temp = []
+    temp1= [0,0]
+    temp2= [0,0]
+    temp3= [0,0]
+
+    #print "Its here too"
+    chosen =  neighbors (start[0],start[1])
+    print chosen
+    #print "Its here too theice"
+    temp1[0] = chosen[0][0]
+    print temp1[0]
+    temp1[1] = chosen[0][1]
+    temp2[0] = chosen[1][0]
+    temp2[1] = chosen[1][1]
+    temp3[0] = chosen[2][0]
+    temp3[1] = chosen[2][1]
+    #print "Its here too twice"
+
+    #calculations for temp1
+    f1 = manhattan_distance(start,temp1)
+    print h[temp1[0]][temp1[1]]
+    print f1
+    #calculations for temp2
+    f2 = manhattan_distance(start,temp2) + h[temp2[0]][temp2[1]]
+    #calculations for temp3
+    f3 = manhattan_distance(start,temp3) + h[temp3[0]][temp3[1]]
+
+    f = min(f1,f2,f3)
+    if f1 == f:
+        print "Chosen node 1"
+        temp = temp1
+    if f2 == f:
+        print "Chosen node 2"
+        temp = temp2
+    if f3 == f:
+        print "Chosen node 3"
+        temp = temp3
+    return temp
+
+
+def a_star(start, goal):
+    frontier = []
+    path = []
+    #print "Yes its here"
+    #neighbors (start[0],start[1])
+    for k in range(4):
+        chosen = choose_neighbor(start)
+        start = chosen
+        path.append(chosen)
+
+    print path
+    return path
+
+
+
+
+
+
+
+
+
+print a_star(S,T)
+
+#if __name__ ="__main__":
