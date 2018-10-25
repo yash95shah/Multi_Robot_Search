1dfcf9d838006902e16b98dcb0fa23e366eb6d1b
Ash <ygshah@asu.edu>
2018-10-24 22:45:19 -0700

    Update mespp.py

--- mespp.py
+++ mespp.py
@@ -20,8 +20,8 @@ neighbors = lambda x, y : [(x2, y2) for x2 in range(x-1, x+2)
                                        (0 <= y2 <= Y))]
 def manhattan_distance (start,neigh):
     distance = abs(start[0]-neigh[0])+ abs(start[1]-neigh[1])
-
     return distance
+
 def choose_neighbor(start):
     temp = []
     temp1= [0,0]
@@ -30,10 +30,8 @@ def choose_neighbor(start):
 
     #print "Its here too"
     chosen =  neighbors (start[0],start[1])
-    print chosen
     #print "Its here too theice"
     temp1[0] = chosen[0][0]
-    print temp1[0]
     temp1[1] = chosen[0][1]
     temp2[0] = chosen[1][0]
     temp2[1] = chosen[1][1]
@@ -43,8 +41,6 @@ def choose_neighbor(start):
 
     #calculations for temp1
     f1 = manhattan_distance(start,temp1)
-    print h[temp1[0]][temp1[1]]
-    print f1
     #calculations for temp2
     f2 = manhattan_distance(start,temp2) + h[temp2[0]][temp2[1]]
     #calculations for temp3
@@ -62,18 +58,29 @@ def choose_neighbor(start):
         temp = temp3
     return temp
 
+def update_heuristic(path):
+    for j in range(len(path)):
+        a =[0,0]
+        a[0] = path[j][0]
+        a[1] = path[j][1]
+
+        h[a[0]][a[1]] = h[S[0]][S[1]] - manhattan_distance (S,a)
+        print manhattan_distance(S,a)
+        print h
 
 def a_star(start, goal):
     frontier = []
     path = []
     #print "Yes its here"
     #neighbors (start[0],start[1])
-    for k in range(4):
+    while start != goal:
         chosen = choose_neighbor(start)
         start = chosen
         path.append(chosen)
 
-    print path
+
+    update_heuristic(path)
+
     return path
 
 
